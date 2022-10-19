5e530109bd7e1c28012cbd2ddcbf54ffb39d4633
Hariharan148 <harirevorhustle@gmail.com>
2022-10-19 23:51:54 +0530

    admin login-1 done

--- quasar-project/src/pages/LoginPage.vue
+++ quasar-project/src/pages/LoginPage.vue
@@ -1,58 +1,66 @@
 <template>
-  <div class="q-pa-md">
-    <div class="q-gutter-y-md" style="max-width: 600px">
-      <q-tabs
-        v-model="tab"
-        class="text-teal"
-      >
-        <q-tab name="mails" icon="mail" label="Mails" />
-        <q-tab name="alarms" icon="alarm" label="Alarms" />
-        <q-tab name="movies" icon="movie" label="Movies" />
-      </q-tabs>
+<q-layout class="dark-bg">
+  <q-page-container>
+    <q-page  class="ma-y-40  vertical-align" >
+      <q-card class="my-card semidark-bg br-primary pa-interior">
+      <q-card-section class=" q-pa-none">
+        <p class="font-regular text-white text-center">Department</p>
+          <div class="q-mt-md">
+            <q-btn-dropdown padding="6px 0px" size="18px" class="grey-bg button font-semi-medium shadow-6 " no-caps  align="center" label="Choose" icon="" dropdown-icon='none'>
+              <q-list separator bordered class="br-primary">
+                <q-item clickable v-close-popup @click="onItemClick" class="button">
+                  <q-item-section>
+                    <q-item-label>Photos</q-item-label>
+                  </q-item-section>
+                </q-item>
 
-      <q-tabs
-        v-model="tab"
-        inline-label
-        class="bg-purple text-white shadow-2"
-      >
-        <q-tab name="mails" icon="mail" label="Mails" />
-        <q-tab name="alarms" icon="alarm" label="Alarms" />
-        <q-tab name="movies" icon="movie" label="Movies" />
-      </q-tabs>
+                <q-item clickable v-close-popup @click="onItemClick">
+                  <q-item-section>
+                    <q-item-label>Videos</q-item-label>
+                  </q-item-section>
+                </q-item>
 
-      <q-tabs
-        v-model="tab"
-        no-caps
-        class="bg-orange text-white shadow-2"
-      >
-        <q-tab name="mails" label="Mails" />
-        <q-tab name="alarms" label="Alarms" />
-        <q-tab name="movies" label="Movies" />
-      </q-tabs>
+                <q-item clickable v-close-popup @click="onItemClick">
+                  <q-item-section>
+                    <q-item-label>Articles</q-item-label>
+                  </q-item-section>
+                </q-item>
+              </q-list>
+            </q-btn-dropdown>
+          </div>
+        <p class="font-regular text-white text-center" style="margin-top:18px;" >Name</p>
+          <div class="q-mt-md ">
+            <q-btn-dropdown padding="6px 0px" size="18px" class="grey-bg button font-semi-medium shadow-6" no-caps  align="center" label="Choose" icon="" dropdown-icon='none' >
+              <q-list separator bordered class="br-primary">
+                <q-item clickable v-close-popup @click="onItemClick" class="button">
+                  <q-item-section>
+                    <q-item-label>Photos</q-item-label>
+                  </q-item-section>
+                </q-item>
 
-      <q-tabs
-        v-model="tab"
-        class="bg-teal text-yellow shadow-2"
-      >
-        <q-tab name="mails" icon="mail" />
-        <q-tab name="alarms" icon="alarm" />
-        <q-tab name="movies" icon="movie" />
-      </q-tabs>
+                <q-item clickable v-close-popup @click="onItemClick">
+                  <q-item-section>
+                    <q-item-label>Videos</q-item-label>
+                  </q-item-section>
+                </q-item>
 
-      <q-tabs
-        v-model="tab"
-        inline-label
-        class="bg-primary text-white shadow-2"
-      >
-        <q-tab name="mails" icon="mail" label="Mails" />
-        <q-tab name="alarms" icon="alarm" label="Alarms" />
-        <q-tab name="movies" icon="movie" label="Movies" />
-        <q-tab name="photos" icon="photo" label="Photos" />
-        <q-tab name="videos" icon="slow_motion_video" label="Videos" />
-        <q-tab name="addressbook" icon="people" label="Address Book" />
-      </q-tabs>
-    </div>
-  </div>
+                <q-item clickable v-close-popup @click="onItemClick">
+                  <q-item-section>
+                    <q-item-label>Articles</q-item-label>
+                  </q-item-section>
+                </q-item>
+              </q-list>
+            </q-btn-dropdown>
+          </div>
+          <div style="margin-top:46px;">
+            <q-btn padding="8px 0px" size="25px" class="bg-prime fw-bold button"  text-color="black" label="NEXT" />
+          </div>
+
+      </q-card-section>
+    </q-card>
+    </q-page>
+  </q-page-container>
+</q-layout>
 </template>
 
 <script>
@@ -61,6 +69,44 @@
 import { defineComponent } from 'vue'
 
 export default defineComponent({
-  name: 'LoginPage'
+  name: 'LoginPage',
+  setup () {
+    return {
+      onItemClick () {
+        // console.log('Clicked on an Item')
+      }
+    }
+  }
 })
 </script>
+
+
+<style scoped>
+
+.my-card{
+  width: 100%;
+  height: 353px;
+  /* max-width: 250px; */
+}
+
+.vertical-align{
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+
+.text-center{
+  text-align: center;
+}
+
+.button{
+  width: 100%;
+  display: flex;
+  align-items: center;
+
+}
+
+
+
+</style>
