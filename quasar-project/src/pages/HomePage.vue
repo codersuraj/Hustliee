2a6f09f4650be153cd313594b2097e1cac169d90
Hariharan <harirevorhustle@gmail.com>
2022-10-20 16:06:23 +0530

    Auth HomePage done

--- quasar-project/src/pages/HomePage.vue
+++ quasar-project/src/pages/HomePage.vue
@@ -1,11 +1,19 @@
 <template>
 <q-layout class="dark-bg">
   <q-page-container>
-    <q-page  class="ma-y-40  vertical-align" >
+    <q-page  class="ma-y-40  vertical-align flex " >
+
+      <img src="../assets/Hustlie.png" alt="logo" class="fly" style="top:5%;">
+      <p class="fly text-white font-small" style="top:19%; text-align: center;">வணக்கம்!, Hello, Bonjour, Hola, Guten Tag, Здравствуйте!</p>
       <q-card class="my-card semidark-bg br-primary pa-interior">
       <q-card-section class=" q-pa-none">
-          <div style="margin-top:46px;">
-            <q-btn padding="8px 0px" size="25px" class="bg-prime fw-bold button"  text-color="black" label="SIGN IN" />
+        <p class="font-regular text-white text-center ">Login as</p>
+          <div style="margin-top:15px;">
+            <q-btn padding="8px 0px" size="25px" class="bg-prime fw-bold button"  text-color="black" label="PROFESSOR" />
+          </div>
+          <p class="font-regular text-white text-center " style="margin-top:30px;">Login as</p>
+          <div style="margin-top:15px;">
+            <q-btn padding="8px 0px" size="25px" class="grey-bg fw-bold button"  text-color="black" label="STUDENT" />
           </div>
 
       </q-card-section>
@@ -36,5 +44,28 @@ export default defineComponent({
 
 <style scoped>
 
+
+.my-card{
+  width: 100%;
+  height: 290px;
+  /* max-width: 250px; */
+}
+
+.vertical-align{
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.button{
+  width: 100%;
+  display: flex;
+  align-items: center;
+
+}
+
+.fly{
+  position: absolute;
+}
 
 </style>
