cacf46a2594a0b6851da5bf75680583dfc437481
Hariharan148 <harirevorhustle@gmail.com>
2022-10-24 22:31:22 +0530

    home page completed

--- quasar-project/src/components/OverallStatus.vue
+++ quasar-project/src/components/OverallStatus.vue
@@ -1,58 +1,61 @@
 <template>
   <div class="box semidark-bg br-secondary q-mt-md text-white">
-    <h2 class="font-medium " style="text-align:center; line-height: 3.2rem;">Overall status</h2>
+    <h2 class="font-medium" style="text-align: center; line-height: 3.2rem">
+      Overall status
+    </h2>
     <div class="flex justify-evenly q-mb-md">
       <div>
         <div>
-          <p class="font-small" style="text-align:center;" >Total</p>
+          <p class="font-small" style="text-align: center">Total</p>
         </div>
         <div class="card br-secondary dark-bg">
-          <p class="font-medium fw-semibold">10</p>
+          <p class="font-medium fw-semibold">{{ data.total }}</p>
         </div>
       </div>
       <div>
         <div>
-          <p class="font-small" style="text-align:center;">Accepted</p>
+          <p class="font-small" style="text-align: center">Accepted</p>
         </div>
         <div class="card br-secondary bg-secon">
-          <p class="font-medium fw-semibold">10</p>
+          <p class="font-medium fw-semibold">{{ data.accepted }}</p>
         </div>
       </div>
       <div>
         <div>
-          <p class="font-small" style="text-align:center;">Pending</p>
+          <p class="font-small" style="text-align: center">Pending</p>
         </div>
         <div class="card br-secondary bg-prime">
-          <p class="font-medium fw-semibold">10</p>
+          <p class="font-medium fw-semibold">{{ data.pending }}</p>
         </div>
       </div>
       <div>
         <div>
-          <p class="font-small" style="text-align:center;">Rejected</p>
+          <p class="font-small" style="text-align: center">Rejected</p>
         </div>
         <div class="card br-secondary bg-red">
-          <p class="font-medium fw-semibold">10</p>
+          <p class="font-medium fw-semibold">{{ data.rejected }}</p>
         </div>
       </div>
-  
     </div>
   </div>
 </template>
 
 <script>
 export default {
-
-}
+  name: "OverallStatus",
+  props: {
+    data: Object,
+  },
+};
 </script>
 
 <style scoped>
-
-.box{
+.box {
   height: 150px;
   width: 100%;
 }
 
-.card{
+.card {
   display: flex;
   align-items: center;
   justify-content: center;
@@ -62,5 +65,4 @@ export default {
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
   text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
 }
-
 </style>
--- quasar-project/src/pages/HomePage.vue
+++ quasar-project/src/pages/HomePage.vue
@@ -1,71 +1,82 @@
 <template>
-<q-layout class="dark-bg">
-  <q-page-container>
-    <q-page  class="ma-y-40  vertical-align flex " >
-
-      <img src="../assets/Hustlie.png" alt="logo" class="fly" style="top:5%;">
-      <p class="fly text-white font-small" style="top:19%; text-align: center;">வணக்கம்!, Hello, Bonjour, Hola, Guten Tag, Здравствуйте!</p>
-      <q-card class="my-card semidark-bg br-primary pa-interior">
-      <q-card-section class=" q-pa-none">
-        <p class="font-regular text-white text-center ">Login as</p>
-          <div style="margin-top:15px;">
-            <q-btn padding="8px 0px" size="25px" class="bg-prime fw-bold button"  text-color="black" label="PROFESSOR" />
-          </div>
-          <p class="font-regular text-white text-center " style="margin-top:30px;">Login as</p>
-          <div style="margin-top:15px;">
-            <q-btn padding="8px 0px" size="25px" class="grey-bg fw-bold button"  text-color="black" label="STUDENT" />
-          </div>
-
-      </q-card-section>
-    </q-card>
-    </q-page>
-  </q-page-container>
-</q-layout>
+  <q-layout class="dark-bg">
+    <q-page-container>
+      <q-page class="ma-y-40 vertical-align flex">
+        <img
+          src="../assets/Hustlie.png"
+          alt="logo"
+          class="fly"
+          style="top: 5%"
+        />
+        <p
+          class="fly text-white font-small"
+          style="top: 19%; text-align: center"
+        >
+          வணக்கம்!, Hello, Bonjour, Hola, Guten Tag, Здравствуйте!
+        </p>
+        <q-card class="my-card semidark-bg br-primary pa-interior">
+          <q-card-section class="q-pa-none">
+            <p class="font-regular text-white text-center">Login as</p>
+            <div style="margin-top: 15px">
+              <q-btn
+                padding="8px 0px"
+                size="25px"
+                class="bg-prime fw-bold button"
+                text-color="black"
+                label="PROFESSOR"
+              />
+            </div>
+            <p
+              class="font-regular text-white text-center"
+              style="margin-top: 30px"
+            >
+              Login as
+            </p>
+            <div style="margin-top: 15px">
+              <q-btn
+                padding="8px 0px"
+                size="25px"
+                class="grey-bg fw-bold button"
+                text-color="black"
+                label="STUDENT"
+              />
+            </div>
+          </q-card-section>
+        </q-card>
+      </q-page>
+    </q-page-container>
+  </q-layout>
 </template>
 
 <script>
-
-
-import { defineComponent } from 'vue'
-
-
+import { defineComponent } from "vue";
 
 export default defineComponent({
-  name: 'LoginPage',
-  data() {
-
-  },
-
-
-
-})
+  name: "LoginPage",
+  data() {},
+});
 </script>
 
-
 <style scoped>
-
-
-.my-card{
+.my-card {
   width: 100%;
   height: 290px;
   /* max-width: 250px; */
 }
 
-.vertical-align{
+.vertical-align {
   display: flex;
   align-items: center;
   justify-content: center;
 }
 
-.button{
+.button {
   width: 100%;
   display: flex;
   align-items: center;
-
 }
 
-.fly{
+.fly {
   position: absolute;
 }
-
 </style>
