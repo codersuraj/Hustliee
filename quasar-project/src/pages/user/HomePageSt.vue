00ffb567fe535e7827a64a910a06761946cd3c64
Hariharan <harirevorhustle@gmail.com>
2023-01-01 21:01:24 +0530

    home

--- quasar-project/src/pages/user/HomePageSt.vue
+++ quasar-project/src/pages/user/HomePageSt.vue
@@ -1,8 +1,9 @@
 <template>
   <div>
     <p class="text-white q-mt-lg">Apply for an OD</p>
-    <q-card class="my-card semidark-bg q-mt-md br-primary">
-      <q-card-section style="padding: 20px 25px">
+    <q-card class="my-card semidark-bg q-mt-md" style="border-radius:10px">
+
+      <q-card-section style="padding: 25px 25px">
         <p class="font-xsm fw-medium text-white text-start">Name</p>
         <div class="q-mt-sm">
           <div class="q-px-md black-r-bg input br-secondary">
@@ -10,7 +11,7 @@
               ref="inputRef"
               :rules="[myRule]"
               borderless
-              v-model="data.name"
+              v-model="name"
               readonly
             >
             </q-input>
@@ -23,7 +24,7 @@
               ref="inputRef"
               :rules="[myRule]"
               borderless
-              v-model="data.roll_no"
+              v-model="rollno"
               readonly
             >
             </q-input>
@@ -74,7 +75,7 @@
                       ref="inputRef"
                       :rules="[myRule]"
                       borderless
-                      v-model="this.period"
+                      v-model="period"
                       class="q-ml-md"
                     >
                     </q-input>
@@ -86,18 +87,18 @@
                 <div class="q-pa-md">
                   <q-input
                     filled
+                    
                     v-model="qdate.to"
                     class="black-r-bg br-secondary"
                   >
                     <template v-slot:prepend>
-                      <q-icon name="event" class="cursor-pointer">
+                      <q-icon name="event" class="cursor-pointer" @click="reset()">
                         <q-popup-proxy
                           cover
                           transition-show="scale"
                           transition-hide="scale"
                         >
                           <div class="q-pa-md" style="text-align: center">
-                            <div class="q-pb-sm">Model: {{ qdate }}</div>
 
                             <q-date
                               v-model="qdate"
@@ -196,19 +197,30 @@
             </q-list>
           </q-btn-dropdown>
         </div>
+          <div>
+          <p
+            v-show="this.denied"
+            class="text-center text-red-4 font-regular q-mt-md"
+          >
+            {{ issue }}
+          </p>
+        </div>
+        <div class="flex items-aligned justify-center">
         <q-btn
-          padding="5px 29px"
-          size="12px"
-          v-model="otp"
-          class="box-shadow bg-prime fw-medium br-primary"
+          padding="5px 0px"
+          size="20px"
+          @click="submitForm()"
+          class="box-shadow bg-prime fw-bold button2"
           style="margin-top: 35px"
           text-color="black"
-          label="Accept"
+          label="APPLY"
           no-caps
         />
+        </div>
       </q-card-section>
     </q-card>
-  </div>
+    </div>
+
 </template>
 
 <script>
@@ -216,24 +228,45 @@ import {db, auth } from "../../../firestore/firestore";
 import { ref } from "vue";
 import firebase from "firebase";
 import { format } from "date-fns";
+import { useVuelidate } from "@vuelidate/core";
+import { required, minLength } from "@vuelidate/validators";
 
 export default {
   data() {
     return {
+      v$: useVuelidate(),
       name: "",
       rollno: "",
       data: "",
       section: "",
-      expiresIn:null,
       chief_mentor: "",
       faculty_list: [],
       mentor: "",
-      chiefMentor:"",
+      mentorID: "",
+      chiefMentor: "",
+      chiefMentorID: "",
+      period: "",
+      purpose: "",
+      denied: false,
+      issue: "",
+    };
+  },
+  validations() {
+    return {
+      name: {
+        required,
+      },
+      rollno: { required },
+      section: { required },
+      purpose: { required },
+      mentor: { required },
+      chiefMentor: { required },
     };
   },
   setup() {
     const inputRef = ref(null);
     return {
+
       model: ref(""),
       year: ref(""),
       dept_name: ref(""),
@@ -258,32 +291,60 @@ export default {
         return format(v, new Date());
       };
     },
-  },
-  watch: {
-    qdate(newDate, oldDate) {
-      if (newDate) {
-        this.Dateformat(newDate)
-      } else {
-        this.Dateformat(oldDate)
-      }
-    },
+    _hours() {
+      return 60000 * 60
+    }
 
   },
+
   methods: {
+    submitForm() {
+      this.v$.$validate();
+
+      if (this.v$.$error) {
+        this.denied = true;
+        this.issue = "All fields are required!"
+      } else {
+        console.log(this.period != "" || this.qdate.to != "");
+        if (this.period != "" || this.qdate.to != "") {
+          if (this.period == "") {
+            this.period = `${this.qdate.from} - ${this.qdate.to}`
+          }
+          this.denied = false;
+          this.applyOd()
+        } else {
+            this.denied = true;
+            this.issue = "All fields are required!"
+        }
+      }
+    },
+    reset() {
+      this.period = ""
+     },
+      
     mentorClick(e) {
       this.mentor = e.target.innerHTML;
+      var result = this.faculty_list.find(item => item.name === this.mentor);
+      this.mentorID = result.id
+      console.log(this.mentorID);
     },
     chiefMentorClick(e) {
       this.chiefMentor = e.target.innerHTML;
+      var result = this.faculty_list.find(item => item.name === this.chiefMentor);
+      this.chiefMentorID = result.id
+      console.log(this.chiefMentorID);
     },
     async getUserData() {
       var docRef = db.collection("students").doc(firebase.auth().currentUser.uid);
 
       await docRef.get().then((doc) => {
+          this.show = true
           if (doc.exists) {
             console.log("Document data:", doc.data());
             this.data = doc.data()
             this.section = this.data.dept + "-" + this.data.section
+            this.name = this.data.name
+            this.rollno = this.data.roll_no
             console.log(this.data);
               this.getMentors()
           } else {
@@ -294,24 +355,93 @@ export default {
           console.log("Error getting document:", error);
       });
     },
-    Dateformat(date) {
-      console.log(date.to);
-      let Todate = date.to
-      const mydate = Todate.split("/");
-      let newDate = `${mydate[0]}-${mydate[1]}-${mydate[2]} 0:0:0.0`
-      var millis = Date.parse(newDate);
-      this.expiresIn = millis - Date.now()
-      console.log(this.expiresIn);
-      if (this.expiresIn < 0) {
-        
+    Dateformat(date, period) {
+      if (!period) {
+        let Todate = date.to
+        const mydate = Todate.split("/");
+        let newDate = `${mydate[0]}-${mydate[1]}-${mydate[2]} 0:0:0.0`
+        var millis = Date.parse(newDate);
+        this.timer(distance)
+        console.log(millis);
+
+      } else {
+          let newDate = date
+        var millis = Date.parse(newDate);
+          
+          this.timer(millis)
       }
-      setTimeout(this.odExpired(), this.expiresIn);
-      
+
+      console.log(this.expiresIn);
 
     },
-    getMentors() {
+    timer(end) {
+      const timer = setInterval(() => { 
+        const now = Date.now()
+        let distance = end - Date.now()
+        console.log(distance);
+        if (end < 0) {
+          clearInterval(timer)
+          this.odExpired()
+          return
+        }
+
+        this.expiresIn = Math.floor( (distance/ this._hours)/2)
+      }, 1000)
+    },
+    // async setData() {
+    //   await db.collection("history").doc().set({
+    //     id: this.odData.id,
+    //     name: this.odData.name,
+    //     roll_no: this.odData.roll_no,
+    //     section: this.odData.section,
+    //     period: this.odData.period,
+    //     purpose: this.odData.purpose,
+    //     mentor: this.odData.mentor,
+    //     chief_mentor: this.odData.chief_mentor,
+    //     status: this.odData.status,
+    //   })
+    //     .then((docRef) => {
+    //     this.deleteOD()
+    //     console.log("Document written with ID: ", docRef);
+        
+          
+    //   })
+    //   .catch((error) => {
+    //       console.error("Error adding document: ", error);
+    //   });
+    // },
+    // async deleteOD() {
+    //   await db.collection("od").doc(this.odID).delete().then(() => {
+    //   console.log("Document successfully deleted!");
+    //   }).catch((error) => {
+    //       console.error("Error removing document: ", error);
+    //   });
+    // },
+    // async odExpired() {
+    //   console.log("inside od", this.odData );
+    //   this.show = true
+    //   await db.collection("od").where("id", "==", firebase.auth().currentUser.uid)
+    //     .get()
+    //     .then((querySnapshot) => {
+
+    //       console.log("emp", querySnapshot.empty);
+    //       querySnapshot.forEach((doc) => {
+    //         this.odData = doc.data()
+    //         this.odID = doc.id
+    //         this.setData()
+    //       })
+    //     })
+    //   .catch((error) => {
+    //     console.log("Error getting documents: ", error);
+
+    //   });
+    //   console.log(this.odData)
+      
+    //   console.log("hello i ");
+    // },
+    async getMentors() {
       this.faculty_list = []
-      db.collection("staffs").where("dept", "==", "EEE")
+      await db.collection("staffs").where("dept", "==", "EEE")
         .onSnapshot((querySnapshot) => {
           querySnapshot.forEach((doc) => {
             var mentor = { id: doc.id, name: doc.data().name }
@@ -321,23 +451,83 @@ export default {
           });
         console.log(this.faculty_list);
     });
+    },
+    async applyOd() {
+      await db.collection("od").doc().set({
+        id: firebase.auth().currentUser.uid,
+        name: this.name,
+        roll_no: this.rollno,
+        section: this.section,
+        period: this.period,
+        purpose: this.purpose,
+        mentor: this.mentorID,
+        chief_mentor: this.chiefMentorID,
+        status: 0,
+      })
+        .then((docRef) => {
+          this.show = false
+          console.log("apply");
+          if (this.qdate.to != "") {
+            this.Dateformat(this.qdate.to, false)
+          } else {
+              let currentDate = new Date().toJSON().slice(0, 10);
+              var date = `${currentDate} 23:59:59.600Z`
+              this.Dateformat(date, true )
+          }
+          
+           db.collection("info").doc(firebase.auth().currentUser.uid).set({
+              total: +1,
+          })
+        console.log("Document written with ID: ", docRef);
+      })
+      .catch((error) => {
+        console.error("Error adding document: ", error);
+      });
+    },
+    async isThereOD(uid) {
+      await db.collection("od").where("id", "==", uid)
+      .get()
+        .then((querySnapshot) => {
+          if (!querySnapshot.empty) {
+            console.log("emp",querySnapshot.empty);
+          querySnapshot.forEach((doc) => {
+            this.odData = doc.data()
+
+              this.odID = doc.id
+              console.log(doc.id, " => ", this.odData.name);
+            this.show = false
+              this.
+          });
+          } else {
+              console.log("emp");
+              this.show = true
+              this.getUserData()
+          
+        }
+          
+      })
+      .catch((error) => {
+        console.log("Error getting documents: ", error);
+        this.query = false
+        this.show = true
+        this.getUserData()
+
+      });
+
+
     }
   },
-  mounted: function mounted() {
-    
-      if(this.data != "") {
-        
-    }
-  },
+
   beforeMount() {
     auth.onAuthStateChanged((user) => {
       if (user) {
         // User is signed in.
         console.log(user.displayName != null);
         if (user.displayName == null) {
-          this.getUserData()
+          this.isThereOD(user.uid)
           console.log("st home");
           this.$router.push("/user/home")
+
         } else {
           this.$router.push("/staff/home")
         }
@@ -381,6 +571,13 @@ export default {
   min-height: 0px;
   padding: 0px;
 }
+
+.button2 {
+  width: 80%;
+  min-height: 0px;
+  padding: 0px;
+}
+
 .q-item__label {
   height: 30px;
   padding: 6px;
